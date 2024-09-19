8a04418e187aa8b215931e6cb5136178fd95fc5e
sa-ga-r <116742231+sa-ga-r@users.noreply.github.com>
2024-09-19 15:22:45 +0000

    third iter commit

--- textalk.py
+++ textalk.py
@@ -1,19 +1,16 @@
 import streamlit as st
-from TTS.api import TTS
-import soundfile as sf
+import gtts as gTTS
+from io import BytesIO
 
-tts = TTS(model_name='tts_models/en/ljspeech/tacotron2-DDC', progress_bar=True)
-st.title('Text Talk')
-st.write('Write your text here...')
-
-text_input = st.text_area('Enter your text')
-if st.button('Speak...'):
-    if text_input.strip():
-        with st.spinner('Generating audio...'):
-            output_file = 'output.wav'
-            tts.tts_to_file(text=text_input, file_path=output_file)
-            data, samplerate = sf.read(output_file)
-            st.audio(output_file)
-            st.success('Speech generated...')
-    else:
-        st.warning('Please enter text...')
+def main():
+    st.title('Text to speech')
+    text = st.text_area("Enter your text here...")
+    if st.button('Generate speech'):
+        if text:
+            tts = gTTS(text=text, lang='en')
+            audio_bytes = BytesIO
+            tts.writ_to_fp(audio_bytes)
+            audio_bytes.seek(0)
+            st.audio(audio_bytes, format='audio/mp3')
+        else:
+            st.error("Please enter text")           
